e3ce3684f9b5d8a363c551bcd7c0a7751b4c8f14
fabian <fabiannacevvedo@gmail.com>
2026-07-03 11:47:46 -0300

    establecio rutas en archivo movie.routes.js

---
+++ src/routes/movie.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getMovies, getMovieById, createMovie, updateMovie, deleteMovie } from "../controllers/movie.controllers.js";
+
+const router = express.Router();
+
+router.get("/", getMovies);
+router.get("/:id", getMovieById);
+router.post("/", createMovie);
+router.put("/:id", updateMovie);
+router.delete("/:id", deleteMovie);
+
+export default router;

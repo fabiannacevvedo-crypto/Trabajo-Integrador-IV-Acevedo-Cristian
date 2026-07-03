96f23f9b554bf5e13b067ce6a4fd8a510fbf4514
fabian <fabiannacevvedo@gmail.com>
2026-07-03 12:12:50 -0300

    agrego modelo , rutas controladores y validaciones en rama crud-peliculas

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
-import express from "express"
+import express from "express";
+//import movieRoutes from "./src/routes/movie.routes.js";
+import { conectarBD, sequelize } from "./src/config/database.js";
 
+//middlewares
 const app = express();
 
-const PORT = express();
+const PORT = 3000;
 
-app.use(express.json);
 
+app.use(express.json());
+
+// Ruta principal (GET /)
+// Cuando se acceda a http://localhost:3001/
+// responderá con un mensaje en formato JSON
 app.get("/", (req, res) => {
     return res.json({
         message: "Servidor todo listo"
     });
 });
+
+app.listen(PORT, async () => {
+    await conectarBD()
+    console.log("servidor listo")
+})
+
+
+
+
+
+
+
+
+
+// Rutas de películas
+//app.use("/api/movies", movieRoutes);
+
+// Conexión a la base de datos y arranque del servidor
+
+// app.listen(PORT, async () => {
+//   try {
+//     // await sequelize.authenticate();
+//     console.log("Conexión a la base de datos establecida correctamente.");
+//     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+//   } catch (error) {
+//     console.error("Error al conectar con la base de datos:", error);
+//   }
+// });
--- src/config/database.js
+++ src/config/database.js
@@ -11,8 +11,8 @@ export const sequelize = new Sequelize("movies", "root", "", {
 export const conectarBD = async () => {
  try {
    sequelize.authenticate();
+   sequelize.sync()
    console.log("Conexión a la base de datos establecida correctamente.");
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
  } catch (error) {
     console.error("Error al conectar con la base de datos:", error);
   }
